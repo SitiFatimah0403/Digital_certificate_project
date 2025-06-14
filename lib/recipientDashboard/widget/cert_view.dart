21e376bb170d44825b045fb92793e13678e80a3a
ainaelyani <ainakayrull@gmail.com>
2025-06-14 16:04:13 +0800

    viewing cert

---
+++ lib/recipientDashboard/widget/cert_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CertView extends StatelessWidget {
+  const CertView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
