058cf4aaad780ce6b8379e76e7f1349b90a9dcc9
SitiFatimah0403 <223846@student.upm.edu.my>
2025-06-17 22:32:47 +0800

    Adding a logout button and made some changes on the main (but still pending)

--- lib/recipientDashboard/screen/home_screen.dart
+++ lib/recipientDashboard/screen/home_screen.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import '../widget/cert_view.dart';
+import 'package:digital_certificate_project/auth/services/auth_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+   HomeScreen({super.key});
+
+  // ✅ Create an instance of AuthService
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xB5ABAABB),
-        title: const Text("RECIPIENT"),
+        backgroundColor: const Color.fromARGB(181, 42, 38, 51),
+         title: const Text(
+        "RECIPIENT",
+        style: TextStyle(
+        color: Colors.white,
+        fontFamily: 'RobotoMono',
+        fontSize: 20,
+         fontWeight: FontWeight.bold,
+    ),
+  ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout, color: Colors.white,),
+            onPressed: () async {
+              await _authService.signOut(); // ✅ call method from AuthService
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/login',
+                (route) => false, // Clear all previous screens
+              );
+            },
+          ),
+        ],
       ),
       body: ListView(
         padding: const EdgeInsets.only(top: 40, bottom: 20),
@@ -31,7 +56,7 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 15),
-          CertView(), // CertView
+          CertView(),
         ],
       ),
     );
