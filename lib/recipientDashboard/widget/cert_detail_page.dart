2af518368da17c5535ffe6d3ea94a17f1ee83351
ainaelyani <ainakayrull@gmail.com>
2025-06-24 10:48:07 +0800

    fetch pdf path from firebase

--- lib/recipientDashboard/widget/cert_detail_page.dart
+++ lib/recipientDashboard/widget/cert_detail_page.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:pdfx/pdfx.dart';
 
-class CertDetailPage extends StatelessWidget {
+class CertDetailPage extends StatefulWidget {
   final Map<String, dynamic> cert;
 
   const CertDetailPage({super.key, required this.cert});
 
   @override
+  State<CertDetailPage> createState() => _CertDetailPageState();
+}
+
+class _CertDetailPageState extends State<CertDetailPage> {
+  late PdfControllerPinch pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final String pdfFile = widget.cert['file'] ?? 'default.pdf';
+
+    pdfController = PdfControllerPinch(
+      document: PdfDocument.openAsset('assets/pdfs/$pdfFile'),
+    );
+  }
+
+  @override
+  void dispose() {
+    pdfController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(cert['file']),
-      ),
-      body: Center(
-        child: Text('You received this certificate on ${cert['receivedDate']}'),
-      ),
+      appBar: AppBar(title: const Text('View Certificate')),
+      body: PdfViewPinch(controller: pdfController),
     );
   }
 }
