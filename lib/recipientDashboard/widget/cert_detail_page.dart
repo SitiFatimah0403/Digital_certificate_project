4096dfd0cb05b66571ef3a2dcf30b2f515543a3f
ainaelyani <ainakayrull@gmail.com>
2025-06-24 11:12:25 +0800

    to make pdf shareable

--- lib/recipientDashboard/widget/cert_detail_page.dart
+++ lib/recipientDashboard/widget/cert_detail_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pdfx/pdfx.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CertDetailPage extends StatefulWidget {
   final Map<String, dynamic> cert;
@@ -16,7 +20,7 @@ class _CertDetailPageState extends State<CertDetailPage> {
   @override
   void initState() {
     super.initState();
-    final String fileName = widget.cert['file'] ?? 'default.pdf';
+    final fileName = widget.cert['file'] ?? 'default.pdf';
     pdfController = PdfControllerPinch(
       document: PdfDocument.openAsset('assets/pdfs/$fileName'),
     );
@@ -28,10 +32,35 @@ class _CertDetailPageState extends State<CertDetailPage> {
     super.dispose();
   }
 
+  Future<void> sharePdf() async {
+    final fileName = widget.cert['file'] ?? 'default.pdf';
+
+    // Load from assets
+    final byteData = await rootBundle.load('assets/pdfs/$fileName');
+
+    // Save to temp file
+    final tempDir = await getTemporaryDirectory();
+    final tempFile = File('${tempDir.path}/$fileName');
+    await tempFile.writeAsBytes(byteData.buffer.asUint8List());
+
+    // Share the file
+    await Share.shareXFiles([XFile(tempFile.path)], text: 'Check out my certificate!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final certName = widget.cert['Name'] ?? 'Certificate';
+
     return Scaffold(
-      appBar: AppBar(title: Text(widget.cert['Name'] ?? 'View Certificate')),
+      appBar: AppBar(
+        title: Text(certName),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.share_sharp),
+            onPressed: sharePdf,
+          ),
+        ],
+      ),
       body: pdfController == null
           ? const Center(child: CircularProgressIndicator())
           : PdfViewPinch(controller: pdfController!),
